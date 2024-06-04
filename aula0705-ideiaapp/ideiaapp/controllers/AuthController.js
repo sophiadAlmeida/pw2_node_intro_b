32a63bcfd13c366d9ba26dbb379f80ab6ac43cf2
sophiadAlmeida <sophia.almeida3@etec.sp.gov.br>
2024-06-04 20:38:20 -0300

    Aula0406 Estruturação de Projeto para Deploy e Conector Azure

--- aula0705-ideiaapp/ideiaapp/controllers/AuthController.js
+++
@@ -1,8 +0,0 @@
-const User = require('../models/User')
- const bcrypt = require('bcrypt')
-
- module.exports = class UserController{
-    static login(req, res){
-        res.render('auth/login')
-    }
- }
